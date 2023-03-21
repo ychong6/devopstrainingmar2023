a07a4d83c46b83ad2355e536f13761931067b9a7
ychong6 <yvchong2000@hotmail.com>
2023-03-21 08:09:33 +0530

    catalog.cs modified @ 21/03/2023 by user3

--- CatalogAPI/Models/Catalog.cs
+++ CatalogAPI/Models/Catalog.cs
@@ -18,6 +18,8 @@ namespace CatalogAPI.Models
         [Column("Catalog_Name")]
         [Required]
         [StringLength(50)]
+	//user3 modifying the code
+
         public string? CatalogName { get; set; }
 
         //[JsonIgnore]       
